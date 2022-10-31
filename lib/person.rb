49de8086b3b5ccedf9ce44214796ee8c66d12de3
saidsoud72 <said.omar@student.moringaschool.com>
2022-10-31 12:57:19 +0300

    redone

--- lib/person.rb
+++ lib/person.rb
@@ -1,26 +1,78 @@
-require 'pry'
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
 
-class String
-
-  def sentence?
-    self.end_with? "."
+  def initialize(name)
+      @name = name
+      @happiness = 8
+      @bank_account = 25
+      @hygiene = 8
   end
 
-  def question?
-    self.end_with? "?"
+  def happiness=(num)
+      @happiness = if num > 10
+          10
+      elsif num < 0
+          0
+      else
+          num
+      end
   end
 
-  def exclamation?
-    self.end_with? "!"
+  def hygiene=(num)
+      @hygiene = if num > 10
+          10
+      elsif num < 0
+          0
+      else
+          num
+      end
   end
 
-  def count_sentences
-    count=self.split(/[.?!]+/).count
-    if count==0
-      return 0
-    else
-      return count
-    end
-  
+  def get_paid(amount)
+      self.bank_account += amount
+      "all about the benjamins"
   end
+
+  def take_bath
+      self.hygiene += 4
+      "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+      self.hygiene -= 3
+      self.happiness += 2
+      "♪ another one bites the dust ♫"
+  end
+
+  def clean?
+      self.hygiene > 7
+  end
+
+  def happy?
+      self.happiness > 7
+  end
+
+  def call_friend(friend)
+      self.happiness += 3
+      friend.happiness += 3
+      "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+      if topic == "politics"
+          [self, friend].each do |person|
+              person.happiness -= 2
+          end
+          "blah blah partisan blah lobbyist"
+      elsif topic == "weather"
+          [self, friend].each do |person|
+              person.happiness += 1
+          end
+          "blah blah sun blah rain"
+      else
+          "blah blah blah blah blah"
+      end
+  end
+
 end
